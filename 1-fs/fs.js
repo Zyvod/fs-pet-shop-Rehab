03fe6b0946dee2b63a33729811631500e8ed38df
Alexander_Fields <Zyvod1300@yahoo.com>
2023-11-01 12:01:24 -0500

    Finished write functionality

--- 1-fs/fs.js
+++ 1-fs/fs.js
@@ -21,7 +21,7 @@ function read() {
         if(error){
             console.log(error)
         } else {
-            if (index === undefined){
+            if (!index){
                 console.log(JSON.parse(data))
                 console.log("It thinks it's not here")
             } else if(typeof index === 'number' && index < JSON.parse(data).length ){
@@ -34,6 +34,33 @@ function read() {
 }
 
 function create() {
-  fs.writeFile('../pets.json', `${process.argv[3]}`)
-}
+ let jsData;
+   if (process.argv.length == 6) {
+     fs.readFile('../pets.json', 'utf8', function(error,data){
+       if (error) {
+         throw error
+          } else {
+            jsData = JSON.parse(data)
+            let age = parseInt(process.argv[3]);
+            let kind = process.argv[4];
+            let name = process.argv[5];
+              if (age.isInteger && kind && name) {
+                let newRecord = { age, kind, name};
+                jsData.push(newRecord)
+                fs.writeFile('../pets.json', `${JSON.stringify(jsData)}`, function(error) {
+                  if(error) {
+                   throw error
+                  } else {
+                    console.log('check JSON File')
+                  }
+                })
 
+              } else {
+                console.error("Usage: node fs.js create <age> <kind> <name>");
+                process.exit(1);
+              }
+          }
+       })
+
+    }
+}
