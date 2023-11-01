807fe497d17453cc4ced5c15679b8324a35c94ba
Alexander_Fields <Zyvod1300@yahoo.com>
2023-11-01 09:00:54 -0500

    Added UI messages as well as read/read[i] functionality

--- 1-fs/fs.js
+++ 1-fs/fs.js
@@ -1,1 +1,39 @@
+import fs from 'fs'
+
+if (process.argv.length < 3) {
+  console.error("Usage: node fs.js [read | create | update | destroy]");
+  process.exit(1);
+}
+
+const subcommand = process.argv[2];
+
+if(subcommand === "read") {
+  read();
+}
+
+if(subcommand === "create") {
+  create();
+}
+
+function read() {
+  let index = parseInt(process.argv[3])
+  fs.readFile('../pets.json', 'utf8', function(error, data){
+        if(error){
+            console.log(error)
+        } else {
+            if (index === undefined){
+                console.log(JSON.parse(data))
+                console.log("It thinks it's not here")
+            } else if(typeof index === 'number' && index < JSON.parse(data).length ){
+                console.log(JSON.parse(data)[index])
+            } else {
+                console.log("Usage: node fs.js read INDEX")
+            }
+    }
+  })
+}
+
+function create() {
+  fs.writeFile('../pets.json', `${process.argv[3]}`)
+}
 
