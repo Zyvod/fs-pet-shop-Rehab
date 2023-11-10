53583b5fc5af70e0b91a15aa2147df6657ec23c9
Alex Fields <134031883+Zyvod@users.noreply.github.com>
2023-11-09 18:30:50 -0600

    Did the thing, now the thing works

---
+++ 4-restful/Migration.sql
@@ -0,0 +1,8 @@
+CREATE TABLE pets (
+  age INT,
+  kind VARCHAR(255),
+  name VARCHAR(255)
+);
+INSERT INTO pets (age, kind, name) VALUES (
+  10, 'dog', 'JohnCena'
+)
--- 4-restful/restful.js
+++ 4-restful/restful.js
@@ -1,20 +1,20 @@
 import express from 'express'
 import pg from 'pg'
-// const { Pool } = pg;
+const { Pool } = pg;
 import fs from 'fs'
 
 // install express, pg and nodemon
-const PORT = 3000;
+const APIPORT = 3000;
 const app = express();
 
 
-// const pool = new Pool({
-//   user: process.env.DB_USER,//postgres
-//   host: process.env.DB_HOST, //localhost
-//   database:"",
-//   password: process.env.DB_PASSWORD, //postgres pw
-//   port: process.env.PORT || 3000,
-// })
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database:"petshop",
+  password: 'Hebrews1211',
+  port: 9934
+})
 
 
 let petData;
@@ -23,130 +23,138 @@ const dbPath = '../pets.json'
 
 app.use(express.json())
 
+app.get('/pets', async (req,res) => {
+  const result = await pool.query(
+    `SELECT * FROM pets;`)
+    console.log()
+    res.send(result.rows)
+})
+
+app.listen(APIPORT, (req, res) => {
+  console.log('serverlistening on PORT 3000')
+})
 // app.use('/', (req,res next) =>) {
 //   console.log('validation check goes here')
 //   next();
 // }
 
 //GET ALL
-app.get('/pets', async (req, res,) => {
-  petData = await getPets()
-  console.log('get all pets path')
-  res.send(petData)
-})
+// app.get('/pets', async (req, res,) => {
+//   petData = await getPets()
+//   console.log('get all pets path')
+//   res.send(petData)
+// })
 
-//GET ONE
-app.get('/pets/:id', async(req,res)=> {
-  petData = await getPets()
-  const index = req.params.id
-  checkIndexRange(index,res)
-  res.send(petData[idnex])
-})
-
-
-//POST
-app.post('/pets', async(req, res) =>{
-  petData = await getPets()
-  let petDataNew = await writePets(req.body)
-  res.send(petDataNew)
-})
+// //GET ONE
+// app.get('/pets/:id', async(req,res)=> {
+//   petData = await getPets()
+//   const index = req.params.id
+//   checkIndexRange(index,res)
+//   res.send(petData[idnex])
+// })
 
 
-//PUT /PATCH
-app.put('/pets/:id', async(req,res)=> {
-  petData = await getPets();
-  const index = req.params.id
-  let adjustedPetData = await adjustPets(index, req.body)
-  res.send(adjustedPetData)
-})
-//DELETE
-app.delete('/pets/:id', async (req,res) => {
-  petData = await getPets();
-  const index = req.params.id
-  let adjustedPetData = await deletePets(index)
-  res.send(adjustedPetData)
-})
-
-app.use((req, res, next) => {
-  next({message: 'The path you are looking for does not exist', status:404})
-})
-
-app.use((err, req, res, next) => {
-  console.log('unknown route hit')
-  res.status(err.status).json({ error:err})
-})
-
-app.listen(PORT, (req, res) => {
-  console.log('serverlistening on PORT 3000')
-})
-
-function checkIndexRange(index, res){
-  if(index > petData.length){
-    res.send(404, 'Not Found')
-  } else if (index < 0){
-    res.send(404,'Not Found')
-  }
-  console.log("inside check range" , petData[index])
-  }
+// //POST
+// app.post('/pets', async(req, res) =>{
+//   petData = await getPets()
+//   let petDataNew = await writePets(req.body)
+//   res.send(petDataNew)
+// })
 
 
-async function getPets(){
-  try{
-    let data = await fs.promises.readFile(dbPath, 'utf8')
-    petData = JSON.parse(data)
-    return petData
-  } catch(error) {
-    res.send(error)
-  }
-}
+// //PUT /PATCH
+// app.put('/pets/:id', async(req,res)=> {
+//   petData = await getPets();
+//   const index = req.params.id
+//   let adjustedPetData = await adjustPets(index, req.body)
+//   res.send(adjustedPetData)
+// })
+// //DELETE
+// app.delete('/pets/:id', async (req,res) => {
+//   petData = await getPets();
+//   const index = req.params.id
+//   let adjustedPetData = await deletePets(index)
+//   res.send(adjustedPetData)
+// })
 
-async function writePets(obj){
-  if(Object.keys(obj).length > 0 ){
-    console.log(petData)
-    petData.push(obj)
-    fs.writeFile(dbPath, JSON.stringify(petData) , (error) => {
-      if(error){
-        return {message: 'Error writing to the file'}
-      }
-    })
-    return newPetData
-  } else {
-    console.log("Request body cannot be empty")
-  }
-}
+// app.use((req, res, next) => {
+//   next({message: 'The path you are looking for does not exist', status:404})
+// })
 
-async function adjustPets(index, obj){
-  if(Object.keys(obj).length > 0 ){
+// app.use((err, req, res, next) => {
+//   console.log('unknown route hit')
+//   res.status(err.status).json({ error:err})
+// })
 
-    const newPetData = [
-      ...petData.slice(0,index),
-      obj,
-      ...petData.slice(index+1)
-    ]
 
-    fs.writeFile(dbPath, JSON.stringify(newPetData) , (error) => {
-      if(error){
-        return {message: 'Error writing to the file'}
-      }
-    })
-    return petData
-  } else {
-    console.log("Request body cannot be empty")
-  }
-}
 
-async function deletePets(index, obj){
-    const newPetData = [
-      ...petData.slice(0,index),
-      ...petData.slice(index+1)
-    ]
+// function checkIndexRange(index, res){
+//   if(index > petData.length){
+//     res.send(404, 'Not Found')
+//   } else if (index < 0){
+//     res.send(404,'Not Found')
+//   }
+//   console.log("inside check range" , petData[index])
+//   }
 
-    fs.writeFile(dbPath, JSON.stringify(newPetData) , (error) => {
-      if(error){
-        return {message: 'Error writing to the file'}
-      }
-    })
-    return petData
 
-    console.log("Request body cannot be empty")
-}
+// async function getPets(){
+//   try{
+//     let data = await fs.promises.readFile(dbPath, 'utf8')
+//     petData = JSON.parse(data)
+//     return petData
+//   } catch(error) {
+//     res.send(error)
+//   }
+// }
+
+// async function writePets(obj){
+//   if(Object.keys(obj).length > 0 ){
+//     console.log(petData)
+//     petData.push(obj)
+//     fs.writeFile(dbPath, JSON.stringify(petData) , (error) => {
+//       if(error){
+//         return {message: 'Error writing to the file'}
+//       }
+//     })
+//     return newPetData
+//   } else {
+//     console.log("Request body cannot be empty")
+//   }
+// }
+
+// async function adjustPets(index, obj){
+//   if(Object.keys(obj).length > 0 ){
+
+//     const newPetData = [
+//       ...petData.slice(0,index),
+//       obj,
+//       ...petData.slice(index+1)
+//     ]
+
+//     fs.writeFile(dbPath, JSON.stringify(newPetData) , (error) => {
+//       if(error){
+//         return {message: 'Error writing to the file'}
+//       }
+//     })
+//     return petData
+//   } else {
+//     console.log("Request body cannot be empty")
+//   }
+// }
+
+// async function deletePets(index, obj){
+//     const newPetData = [
+//       ...petData.slice(0,index),
+//       ...petData.slice(index+1)
+//     ]
+
+//     fs.writeFile(dbPath, JSON.stringify(newPetData) , (error) => {
+//       if(error){
+//         return {message: 'Error writing to the file'}
+//       }
+//     })
+//     return petData
+
+//     console.log("Request body cannot be empty")
+// }
